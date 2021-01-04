d68866d4ae19e72f215ea1d07c7d06d32e01bb9d
shivaji <official.shivaji007@gmail.com>
2021-01-04 20:57:01 +0530

    Updates

---
+++ precache-manifest.508d40b6ce8efaa05f63e61d175fc74e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c5fd7fef49a3b0f3d4c929c7535b9b7",
+    "url": "/discordMernFrontend/index.html"
+  },
+  {
+    "revision": "820e11f6f5b4e5803000",
+    "url": "/discordMernFrontend/static/css/main.089cb756.chunk.css"
+  },
+  {
+    "revision": "d914113b5745d2ea23c8",
+    "url": "/discordMernFrontend/static/js/2.5fd70d8a.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/discordMernFrontend/static/js/2.5fd70d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "820e11f6f5b4e5803000",
+    "url": "/discordMernFrontend/static/js/main.0d643032.chunk.js"
+  },
+  {
+    "revision": "264299a33337866eafe4",
+    "url": "/discordMernFrontend/static/js/runtime-main.9c08cfb9.js"
+  }
+]);
